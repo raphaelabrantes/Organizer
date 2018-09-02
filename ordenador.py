cdf3b0400aa4c7fbac44d7bc69e0395f37ef66c0
raphaelabrantes <antenabr2@gmail.com>
2018-09-02 19:51:47 -0300

    fix little bugs

--- ordenador.py
+++ ordenador.py
@@ -1,22 +1,27 @@
 import os
 os.system("ls -R > nomescorretos.txt")
 with open("nomescorretos.txt", "r") as filename:
-    lines = filename.readlines()
+	lines = filename.readlines()
 
 files = []
-
+path = ""
 lines = [line.strip() for line in lines]
 for line in lines:
-    if line == ".:" or line == "":
-        pass
-    elif line[:1] == "./":
-        path = line
-    else:
-        if line[-1].lower != "g" or "." not in line:
-            pass
-        elif line=[-1].lower == "g" or "." in line:
-            files.append("%s%s"%(path,line))
+	if line == ".:" or line == "":
+		pass
+	elif "./" in line:
+		path = line[1:]
+		path = path[: len(path) -1] + "/"
+		pass
+	else:
+		if line[-1].lower() != 'g' and line[-1] != "4":
+			pass
+		elif (line[-1].lower() == 'g' or line[-1] == "4") and "." in line:
+			files.append("%s%s"%(path,line))
 for i in files:
-    print(i)
+	os.system("file %s"%(i))
+
+os.system("rm nomescorretos.txt")
+
 
     
