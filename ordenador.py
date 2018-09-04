e478323484643d1f637e0a59e02263ff5b85b380
raphaelabrantes <antenabr2@gmail.com>
2018-09-04 19:11:40 -0300

    maijor changes and fix bugs

--- ordenador.py
+++ ordenador.py
@@ -4,24 +4,26 @@ with open("nomescorretos.txt", "r") as filename:
 	lines = filename.readlines()
 
 files = []
-path = ""
+path = os.getcwd() + "/"
+
 lines = [line.strip() for line in lines]
 for line in lines:
 	if line == ".:" or line == "":
 		pass
 	elif "./" in line:
-		path = line[1:]
+		path = os.getcwd()
+		path += line[1:]
 		path = path[: len(path) -1] + "/"
-		pass
 	else:
 		if line[-1].lower() != 'g' and line[-1] != "4":
 			pass
 		elif (line[-1].lower() == 'g' or line[-1] == "4") and "." in line:
 			files.append("%s%s"%(path,line))
 for i in files:
-	os.system("file %s"%(i))
+	os.system("exiv2 '%s' | grep name >> info.txt" %(i))
+	os.system("exiv2 '%s' | grep timestamp >> info.txt"%(i))
+	os.system("echo  >> info.txt")
+
 
 os.system("rm nomescorretos.txt")
 
-
-    
