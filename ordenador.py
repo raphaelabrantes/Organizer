3c877ff5fe130411fdc2b1755afc9f0363e039e2
raphaelabrantes <antenabr2@gmail.com>
2018-09-06 14:32:39 -0300

    better performance, I deleted some useless funcions and used python commands instead of the previos bash ones to correct performance and some errors

--- ordenador.py
+++ ordenador.py
@@ -1,29 +1,32 @@
 import os
-os.system("ls -R > nomescorretos.txt")
-with open("nomescorretos.txt", "r") as filename:
-	lines = filename.readlines()
-
-files = []
-path = os.getcwd() + "/"
-
-lines = [line.strip() for line in lines]
-for line in lines:
-	if line == ".:" or line == "":
-		pass
-	elif "./" in line:
-		path = os.getcwd()
-		path += line[1:]
-		path = path[: len(path) -1] + "/"
-	else:
-		if line[-1].lower() != 'g' and line[-1] != "4":
+def get_paths():
+	os.system("ls -R > nomescorretos.txt")
+	with open("nomescorretos.txt", "r") as filename:
+		lines = filename.readlines()
+	files = []
+	path = os.getcwd() + "/"
+	lines = [line.strip() for line in lines]
+	for line in lines:
+		if line == ".:" or line == "":
 			pass
-		elif (line[-1].lower() == 'g' or line[-1] == "4") and "." in line:
-			files.append("%s%s"%(path,line))
+		elif "./" in line:
+			path = os.getcwd()
+			path += line[1:]
+			path = path[: len(path) -1] + "/"
+		else:
+			if line[-1].lower() != 'g' and line[-1] != "4":
+				pass
+			elif (line[-1].lower() == 'g' or line[-1] == "4") and "." in line:
+				files.append("%s%s"%(path,line))
+	os.system("rm nomescorretos.txt")
+	return files
+
+
+	
+files = get_paths()
 for i in files:
-	os.system("exiv2 '%s' | grep name >> info.txt" %(i))
-	os.system("exiv2 '%s' | grep timestamp >> info.txt"%(i))
-	os.system("echo  >> info.txt")
+	with open("nomes.txt", "a") as filename:
+		filename.write("%s\n" %(i))
 
-
-os.system("rm nomescorretos.txt")
-
+	
+	
