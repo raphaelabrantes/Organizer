5243b5ab9bd9718a7987d3c3b7a561b009238eed
raphaelabrantes <antenabr2@gmail.com>
2018-09-06 20:34:49 -0300

    Created two new functions called, check_and_save and save_name. check_and_save checks if the the file is a copy or not with if functions, if not a copy, the file is safe using the save_name funtion in a file called nomes.txt, if is a copy, the save_name is called and saves the name in rejected.txt. Also, check_and_save, count the total amount of photos, the amount that was accepted (nomes.txt) and the amount that was rejected. Next I will create better ways of checking if is a copy or not, and move the ones that are not copies to other files, devided by date.

--- ordenador.py
+++ ordenador.py
@@ -1,4 +1,6 @@
 import os
+
+
 def get_paths():
 	os.system("ls -R > nomescorretos.txt")
 	with open("nomescorretos.txt", "r") as filename:
@@ -14,19 +16,48 @@ def get_paths():
 			path += line[1:]
 			path = path[: len(path) -1] + "/"
 		else:
-			if line[-1].lower() != 'g' and line[-1] != "4":
-				pass
-			elif (line[-1].lower() == 'g' or line[-1] == "4") and "." in line:
+			if (line[-3:].lower() == 'jpg' or line[-3:] == "mp4" or line[-3:] == "png") and "." in line:
 				files.append("%s%s"%(path,line))
+	
 	os.system("rm nomescorretos.txt")
 	return files
 
 
-	
-files = get_paths()
-for i in files:
-	with open("nomes.txt", "a") as filename:
-		filename.write("%s\n" %(i))
+def save_name(name, file_n):
+	with open(file_n, "a") as filename:
+		filename.write("%s\n" %(name))
 
-	
-	
+
+def check_and_save(files):
+	saves = 0
+	rejected = 0
+	totais = 0
+	for name in files:
+		totais +=1
+		name_l = name.lower()
+		if ("(1)" not in name_l) and ("(2)" not in name_l) and ("(3)" not in name_l) and ("cópia" not in name_l) and ("_face" not in name_l):
+			if (" 2.jpg" not in name_l) and (" 3.jpg" not in name_l) and (" 4.jpg" not in name_l) and (" 5.jpg" not in name_l) and ("_2.jpg" not in name_l):
+				save_name(name,"nomes.txt")
+				saves += 1
+			else: 
+				rejected +=1
+				save_name(name, "rejected.txt")
+		elif ("(1)" in name_l) or ("(2)" in name_l) or ("(3)" in name_l):
+			if ("amiga" in name_l or "madri" in name_l):
+				save_name(name,"nomes.txt")
+				saves+= 1
+			else:
+				rejected += 1
+				save_name(name, "rejected.txt")
+		else:
+			rejected += 1
+			save_name(name, "rejected.txt")
+
+	print("Salvadas : %d" %(saves))
+	print("Rejeitadas: %d" %(rejected))
+	print("Totais: %d" %(totais))
+
+
+files = get_paths()
+files.sort()
+check_and_save(files)
